22133317b7623803d97acc881356f18a055f4923
Avinash Jaiswal <avinash.jaiswal@incedoinc.com>
2023-05-22 17:04:58 +0530

    removed comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 // import { Store } from '@ngrx/store';
 import { Expense } from './Models/expense';
 import { DataService } from './Service/data.service';
-// import { addMovies, getMovies } from './Store/Actions/movie.action';
 
 @Component({
   selector: 'app-root',
@@ -15,21 +14,18 @@ export class AppComponent implements OnInit {
   title = 'movieApp';
 
   constructor(private dataService: DataService) {}
-  // , private store: Store) {}
 
   ngOnInit(): void {
     this.getAllMovies();
   }
 
   getAllMovies(): void {
-    // this.store.dispatch(getMovies());
     this.dataService.getExpenses().subscribe((movies: Expense[]) => {
       this.expenses = movies;
     });
   }
 
   addNewExpenses(): void {
-    // this.store.dispatch(addMovies(this.newMovie));
     this.dataService.addExpenses(this.newExpense).subscribe((res) => {
       this.getAllMovies();
       this.newExpense = new Expense();
